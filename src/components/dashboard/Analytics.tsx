073f96cd5fa6a775953e48f1faf75cd5d806cc33
Gogo602 <gogomarosi61@gmail.com>
2025-10-26 21:22:50 +0300

    Implementing dashboard

--- src/components/dashboard/Analytics.tsx
+++ src/components/dashboard/Analytics.tsx
@@ -2,7 +2,7 @@ import Card from "./Card";
 
 export default function Analytics() {
   return (
-    <section className="grid grid-cols-3 gap-6 px-20">
+    <section className="grid grid-cols-1 gap-6 md:px-20 md:grid-cols-2 lg:grid-cols-3">
         <Card />
         <Card />
         <Card />
